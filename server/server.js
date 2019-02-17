28835d2b769a1bcd10127e068b76c4cabed6c214
Kumar Vikram <vikram89813@gmail.com>
2019-02-17 20:21:57 +0530

    POST users/login route

--- server/server.js
+++ server/server.js
@@ -114,6 +114,18 @@ app.get('/users/me',authenticate,(req,res)=>{
     res.send(req.user);
 });
 
+app.post('/users/login',(req,res)=>{
+    var body = _.pick(req.body,['email','password']);
+    User.findByCredentials(body.email,body.password).then((user)=>{
+        return user.generateAuthToken()
+        .then((token)=>{
+            res.header('x-auth',token).send(user);
+        });
+    })
+    .catch((e)=>{
+        return res.status(400).send();
+    });
+});
 //start server
 app.listen(port,()=>{
     console.log('Server started on port: ', port);
@@ -122,7 +134,3 @@ app.listen(port,()=>{
 module.exports = {
     app
 };
-
-
-
-
