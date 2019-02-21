d7585bb2af5d87fd99c6b430373b83576db92c9f
kumar.vikram <kumar.vikram@samsung.com>
2019-02-21 17:14:55 +0530

    Begin async await integration

--- server/server.js
+++ server/server.js
@@ -55,22 +55,38 @@ app.get('/todos/:id',authenticate,(req,res)=>{
     });
 });
 
-app.delete('/todos/:id',authenticate,(req,res)=>{
-    var id = req.params.id;
-    if(!ObjectID.isValid(id)) {
-        return res.status(404).send();
-    }
-    Todo.findOneAndRemove({
-        _id: id,
-        _creater: req.user._id
-    }).then((todo)=>{
+app.delete('/todos/:id',authenticate,async (req,res)=>{
+    // var id = req.params.id;
+    // if(!ObjectID.isValid(id)) {
+    //     return res.status(404).send();
+    // }
+    // Todo.findOneAndRemove({
+    //     _id: id,
+    //     _creater: req.user._id
+    // }).then((todo)=>{
+    //     if(!todo) {
+    //         return res.status(404).send();
+    //     }
+    //     res.send({todo});
+    // }).catch((e)=>{
+    //     res.status(400).send();
+    // });
+    try{
+        const id = req.params.id;
+        if(!ObjectID.isValid(id)) {
+            return res.status(404).send();
+        }
+        const todo = await Todo.findOneAndRemove({
+                _id: id,
+                _creater: req.user._id
+            });
         if(!todo) {
             return res.status(404).send();
         }
         res.send({todo});
-    }).catch((e)=>{
+    }catch(e){
         res.status(400).send();
-    });
+    }
 });
 
 app.patch('/todos/:id',authenticate,(req,res)=>{
@@ -102,51 +118,73 @@ app.patch('/todos/:id',authenticate,(req,res)=>{
 });
 
 //Users route.
-app.post('/users',(req,res)=>{
-    var body = _.pick(req.body,['email','password']);
-    var user = new User(body);
+app.post('/users', async (req,res)=>{
+    // var body = _.pick(req.body,['email','password']);
+    // var user = new User(body);
 
-    user.save()
-    .then(()=>{
-         return user.generateAuthToken();
-        //res.send(user);
-    })
-    .then((token)=>{
-        // when we prefix a header with x-something
-        // it becomes a custom header(may not be supported by http)
-        // only for internal use.
+    // user.save()
+    // .then(()=>{
+    //      return user.generateAuthToken();
+    //     //res.send(user);
+    // })
+    // .then((token)=>{
+    //     // when we prefix a header with x-something
+    //     // it becomes a custom header(may not be supported by http)
+    //     // only for internal use.
+    //     res.header('x-auth',token).send(user);
+    // })
+    // .catch((e)=>{
+    //     res.status(400).send(e);
+    // });
+    try{
+        const body = _.pick(req.body,['email','password']);
+        var user = new User(body);
+        await user.save();
+        const token = user.generateAuthToken();
         res.header('x-auth',token).send(user);
-    })
-    .catch((e)=>{
+    }catch(e){
         res.status(400).send(e);
-    });
+    }
 });
 
 app.get('/users/me',authenticate,(req,res)=>{
     res.send(req.user);
 });
 
-app.post('/users/login',(req,res)=>{
-    var body = _.pick(req.body,['email','password']);
-    User.findByCredentials(body.email,body.password).then((user)=>{
-        return user.generateAuthToken()
-        .then((token)=>{
-            res.header('x-auth',token).send(user);
-        });
-    })
-    .catch((e)=>{
-        return res.status(400).send();
-    });
+app.post('/users/login',async (req,res)=>{
+    try{
+        const body = _.pick(req.body,['email','password']);
+        const user = await User.findByCredentials(body.email,body.password);
+        const token = await user.generateAuthToken();
+        res.header('x-auth',token).send(user);
+    }catch(e){
+        res.status(400).send();
+    }
+    // User.findByCredentials(body.email,body.password).then((user)=>{
+    //     return user.generateAuthToken()
+    //     .then((token)=>{
+    //         res.header('x-auth',token).send(user);
+    //     });
+    // })
+    // .catch((e)=>{
+    //     return res.status(400).send();
+    // });
 });
 
-app.delete('/users/me/token',authenticate,(req,res)=>{
-    req.user.removeToken(req.token)
-    .then(()=>{
+app.delete('/users/me/token',authenticate,async (req,res)=>{
+    // req.user.removeToken(req.token)
+    // .then(()=>{
+    //     res.status(200).send();
+    // })
+    // .catch((e)=>{
+    //     res.status(400).send();
+    // });
+    try{
+        await req.user.removeToken(req.token);
         res.status(200).send();
-    })
-    .catch((e)=>{
+    }catch(e){
         res.status(400).send();
-    });
+    }
 });
 
 //start server
